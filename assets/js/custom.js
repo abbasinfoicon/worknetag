3a3b824e238eac5057095d1988e9defbc94fb730
abbasinfoicon <uid3@infoiconsoftware.com>
2023-02-27 18:18:33 +0530

    adf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -78,16 +78,16 @@ $(window).scroll(function () {
             $({ countNum: $this.text() }).animate({
                 countNum: countTo
             },
-            {
-                duration: 2000,
-                easing: 'swing',
-                step: function () {
-                    $this.text(Math.floor(this.countNum));
-                },
-                complete: function () {
-                    $this.text(this.countNum);
-                }
-            });
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+                });
         });
 
         a = 1;
@@ -104,3 +104,22 @@ $('.navbar-toggler').click(function () {
     $('body').toggleClass("overflow-body");
 });
 
+
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function () {
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function () {
+    if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match(
+            /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+    } else {
+        customTxt.innerHTML = "No file chosen, yet.";
+    }
+});
